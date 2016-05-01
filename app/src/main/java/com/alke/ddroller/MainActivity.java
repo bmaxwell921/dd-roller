1ab7da968ea8a350a4354daceb7db1ff70951147
Brandon <bmaxwell921@gmail.com>
2016-05-01 11:07:23 -0700

    Cleaning up style, adding proper drawer tabs

--- app/src/main/java/com/alke/ddroller/MainActivity.java
+++ app/src/main/java/com/alke/ddroller/MainActivity.java
@@ -5,18 +5,13 @@ import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 public class MainActivity extends Activity
     implements NavigationDrawerFragment.NavigationDrawerCallbacks {
@@ -24,24 +19,24 @@ public class MainActivity extends Activity
   /**
    * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
    */
-  private NavigationDrawerFragment mNavigationDrawerFragment;
+  private NavigationDrawerFragment navigationDrawerFragment;
 
   /**
    * Used to store the last screen title. For use in {@link #restoreActionBar()}.
    */
-  private CharSequence mTitle;
+  private CharSequence title;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    mNavigationDrawerFragment = (NavigationDrawerFragment)
+    navigationDrawerFragment = (NavigationDrawerFragment)
         getFragmentManager().findFragmentById(R.id.navigation_drawer);
-    mTitle = getTitle();
+    title = getTitle();
 
     // Set up the drawer.
-    mNavigationDrawerFragment.setUp(
+    navigationDrawerFragment.setUp(
         R.id.navigation_drawer,
         (DrawerLayout) findViewById(R.id.drawer_layout));
   }
@@ -55,39 +50,24 @@ public class MainActivity extends Activity
         .commit();
   }
 
-  public void onSectionAttached(int number) {
-    switch (number) {
-      case 1:
-        mTitle = getString(R.string.title_section1);
-        break;
-      case 2:
-        mTitle = getString(R.string.title_section2);
-        break;
-      case 3:
-        mTitle = getString(R.string.title_section3);
-        break;
-    }
-  }
-
-  public void restoreActionBar() {
-    ActionBar actionBar = getActionBar();
-    actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-    actionBar.setDisplayShowTitleEnabled(true);
-    actionBar.setTitle(mTitle);
-  }
-
-
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
-    if (!mNavigationDrawerFragment.isDrawerOpen()) {
+    if (!navigationDrawerFragment.isDrawerOpen()) {
       // Only show items in the action bar relevant to this screen
       // if the drawer is not showing. Otherwise, let the drawer
       // decide what to show in the action bar.
-      getMenuInflater().inflate(R.menu.main, menu);
+      getMenuInflater().inflate(R.menu.global, menu);
       restoreActionBar();
       return true;
     }
     return super.onCreateOptionsMenu(menu);
+  }
+
+  private void restoreActionBar() {
+    ActionBar actionBar = getActionBar();
+    actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+    actionBar.setDisplayShowTitleEnabled(true);
+    actionBar.setTitle(title);
   }
 
   @Override
@@ -133,16 +113,12 @@ public class MainActivity extends Activity
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
         Bundle savedInstanceState) {
-      View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-      return rootView;
+      return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
     @Override
     public void onAttach(Activity activity) {
       super.onAttach(activity);
-      ((MainActivity) activity).onSectionAttached(
-          getArguments().getInt(ARG_SECTION_NUMBER));
     }
   }
-
 }
